0f594446f7fd4640dacf2ddbf53ed8a73077a4bc
divyanshuraj1095 <divyanshuraj23102005@gmail.com>
2025-06-20 17:32:59 +0530

    acecdotes and part 1 complete

---
+++ Part1/anecdotes/src/Anecdote.jsx
@@ -0,0 +1,13 @@
+const Anecdote = ({text, votesCount}) =>{
+   
+    return(
+        <>
+        <div>
+    <p>{text}</p>
+    <p>has {votesCount} votes</p>
+  </div>
+        </>
+    )
+}
+export default Anecdote
+  
